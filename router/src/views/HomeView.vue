3928abddb76e2b2640124576daa1c1374f661ca3
Akhundzadafatima <165835698+Akhundzadafatima@users.noreply.github.com>
2024-10-29 17:38:04 +0400

    Add files via upload

---
+++ router/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Home Page</h1>
+<router-link to="/hakkinda" >
+    haqqimizda
+</router-link>
+<br>
+<router-link to="/">
+    Ana seyfe
+</router-link>
+    </div>
+</template>
